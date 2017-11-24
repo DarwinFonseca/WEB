f005eb7fcdbb8ec2653268436d644f3225425f96
DarwinFonseca <dfonsecaabr@unimimnuto.edu.co>
2017-11-24 03:34:10 -0500

    API + DisQus
    Final Master

--- resources/views/comentarios.blade.php
+++ resources/views/comentarios.blade.php
@@ -26,7 +26,8 @@
                 <td><a href=http:\\{{$publicacion->link}} target='_blank'> {{ $publicacion->descripcion  }} </a></td>
                 <td>{{$publicacion->name}}</td>
                 <td><a class='glyphicon glyphicon-thumbs-up' href='{{url("/ValidarVoto/{$publicacion->id_publicacion}")}}'/> {{$publicacion->votos}}</td>
-                <td><a class='glyphicon glyphicon-comment' href='{{url("/Comentarios/{$publicacion->id_publicacion}")}}'/> {{$publicacion->comentarios}}</td>
+                <!--td><a class='glyphicon glyphicon-comment' href='{{url("/Comentarios/{$publicacion->id_publicacion}")}}'/> {{$publicacion->comentarios}}</td  DISQUS-->
+                <td><a class='glyphicon glyphicon-comment' href='{{url("/Comentarios/{$publicacion->id_publicacion}")}}'/> Comentar</td>
               </tr>
               @endforeach
               @else
@@ -40,6 +41,8 @@
         @endif
 
       </div>
+<!--
+Omitido por DISQUS
 
     @guest
     Para interactuar con el contenido debe <a href="{{url('login')}}">Iniciar Sesión</a><br>
@@ -72,10 +75,34 @@
             @endguest
             @endif
 
-          </div>
+          </div-->
+
+<!--//DISQUS Añadido-->
+          <div id="disqus_thread"></div>
+          <script>
+
+          /**
+          *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+          *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+          /*
+          var disqus_config = function () {
+          this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+          this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+          };
+          */
+          (function() { // DON'T EDIT BELOW THIS LINE
+          var d = document, s = d.createElement('script');
+          s.src = 'https://webforo.disqus.com/embed.js';
+          s.setAttribute('data-timestamp', +new Date());
+          (d.head || d.body).appendChild(s);
+          })();
+          </script>
+          <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 
 
-        </div>
-
+        </div> <!--
+        Div Container Principal
+        Script de Disqus-->
+        <script id="dsq-count-scr" src="//webforo.disqus.com/count.js" async></script>
     </body>
 @endsection
